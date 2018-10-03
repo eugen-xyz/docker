aae7921309618d6f2ca061996b9abb8e1377c580
Eugene Grava <egrava@i4asiacorp.com>
2018-10-03 15:14:22 +0800

    command to create self-signed certificate
    Signed-off-by: Eugene Wang <dev.eugene.wang@gmail.com>

---
+++ certs/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * [command for creating self-signed certificate]
+ *
+ * sudo openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out localhost.crt -keyout localhost.key
+ *
+ */
